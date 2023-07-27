3052b868e53ceed71e6522d3d653c0334a06e919
kaponero <kaponero@gmail.com>
2023-07-27 18:11:07 -0300

    fixed paginajurados + carousel video

--- index.py
+++ index.py
@@ -28,12 +28,12 @@ def inscripcion():
     else:
         return render_template('/inscripcion.html')
     return render_template('/inscripcion.html')
-
+programa=['aca' 'va' 'un' 'nombre']
 @app.route('/paginajurados') 
 def jurados(): 
     return render_template('/paginajurados.html', 
-                           programa="programa_largo", 
-                           genero="genero",
+                           programa="Hello World", 
+                           genero="comedia fantastica",
                            categoria="categoria",
                            vivo="vivo",
                            localidad="localidad",
@@ -48,19 +48,32 @@ def jurados():
                            conductor="conductor", 
                            protagonista="protagonista", 
                            cronistas="cronistas", 
-                           otros="otros",
+                           otros="otros locos",
                            nombre="nombre",
                            direccion="direccion",
                            localidad_canal="localidad canal",
                            contacto="contacto",
                            telefono="telefono",
                            email="email",
-                           socio="socio"
+                           socio="socio",
+                           razonsocial="razon social",
+                           cuit="cuit",
+                           observacion="observacion 1",
+                           estado="estado ok",
+                           cover="https://drive.google.com/file/d/1XBPO992XLspRVgePVEV5_dIN9JiXHbfj/preview",
+                           programa1="https://drive.google.com/file/d/1XBPO992XLspRVgePVEV5_dIN9JiXHbfj/preview",
+                           programa2="https://drive.google.com/file/d/1XBPO992XLspRVgePVEV5_dIN9JiXHbfj/preview",
+                           programa3="https://drive.google.com/file/d/1XBPO992XLspRVgePVEV5_dIN9JiXHbfj/preview"
+
                            ) 
 
 @app.route('/login') 
 def loguin(): 
     return render_template('/login2.html') 
+
+@app.route("/hello-world")
+def hello_world():
+    return render_template("page-1.html", title="Hello World")
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000, debug=True) 
