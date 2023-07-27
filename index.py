5e48defe7326f47880757cc00007aca6caf7a23c
kaponero <kaponero@gmail.com>
2023-07-26 22:31:12 -0300

    segundo domit

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template
+import form 
 app = Flask(__name__) 
 
 @app.route('/') 
@@ -30,7 +31,36 @@ def inscripcion():
 
 @app.route('/paginajurados') 
 def jurados(): 
-    return render_template('/paginajurados.html', programa="Hola", genero="DEPORTIVO") 
+    return render_template('/paginajurados.html', 
+                           programa="programa_largo", 
+                           genero="genero",
+                           categoria="categoria",
+                           vivo="vivo",
+                           localidad="localidad",
+                           emision="emision",
+                           duracion="duracion",
+                           productor="productor",
+                           coproductor="coproductor", 
+                           autor="autor", 
+                           director="director", 
+                           camara="camara", 
+                           sonido="sonido", 
+                           conductor="conductor", 
+                           protagonista="protagonista", 
+                           cronistas="cronistas", 
+                           otros="otros",
+                           nombre="nombre",
+                           direccion="direccion",
+                           localidad_canal="localidad canal",
+                           contacto="contacto",
+                           telefono="telefono",
+                           email="email",
+                           socio="socio"
+                           ) 
+
+@app.route('/login') 
+def loguin(): 
+    return render_template('/login2.html') 
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000, debug=True) 
