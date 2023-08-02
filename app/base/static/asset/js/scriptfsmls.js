486fc81b22f79cfc112086d2d9e46d35cb3bbb14
kaponero <kaponero@gmail.com>
2023-08-02 17:26:01 -0300

    validacion de formulario

--- app/base/static/asset/js/scriptfsmls.js
+++ app/base/static/asset/js/scriptfsmls.js
@@ -4,6 +4,7 @@ var form_3 = document.querySelector(".form_3");
 var form_4 = document.querySelector(".form_4");
 var form_5 = document.querySelector(".form_5");
 
+var form = document.querySelector("form")
 
 var form_1_btns = document.querySelector(".form_1_btns");
 var form_2_btns = document.querySelector(".form_2_btns");
@@ -30,85 +31,98 @@ var btn_done = document.querySelector(".btn_done");
 var modal_wrapper = document.querySelector(".modal_wrapper");
 var shadow = document.querySelector(".shadow");
 
-form_1_next_btn.addEventListener("click", function(){
-	
+const forms = document.querySelectorAll('.needs-validation')
+form.addEventListener('click', event => {
+	if (!form.checkValidity()) {
+	event.preventDefault()
+	event.stopPropagation()
+	}
+	form.classList.add('was-validated')
+}, false)
 
+form_1_next_btn.addEventListener("click", function(){
+	var selectcat = document.getElementById('categorias')
+	var selectgen = document.getElementById('genero')
+	var selectviv = document.getElementById('vivo')
+	
+	if (selectcat.value  == 0 || selectgen.value==0 || selectviv.value==0){
+		alert ("Complete los campos")
+	}
+	else{
 		form_1.style.display = "none";
 		form_2.style.display = "block";
 		form_1_btns.style.display = "none";
 		form_2_btns.style.display = "flex"; 
-		form_2_progessbar.classList.add("active");
-	
+		form_2_progessbar.classList.add("active");}
 });
 
 form_2_back_btn.addEventListener("click", function(){
 
-		form_1.style.display = "block";
-		form_2.style.display = "none";
-		form_1_btns.style.display = "flex";
-		form_2_btns.style.display = "none";
-		form_2_progessbar.classList.remove("active");
+	form_1.style.display = "block";
+	form_2.style.display = "none";
+	form_1_btns.style.display = "flex";
+	form_2_btns.style.display = "none";
+	form_2_progessbar.classList.remove("active");
 	
 });
 
 form_2_next_btn.addEventListener("click", function(){
+	
 	form_2.style.display = "none";
 	form_3.style.display = "block";
-
 	form_3_btns.style.display = "flex";
 	form_2_btns.style.display = "none";
-
 	form_3_progessbar.classList.add("active");
+	
 });
 
 form_3_back_btn.addEventListener("click", function(){
+
 	form_2.style.display = "block";
 	form_3.style.display = "none";
-
 	form_2_btns.style.display = "flex";
 	form_3_btns.style.display = "none";
-
 	form_3_progessbar.classList.remove("active");
 });
 
 form_3_next_btn.addEventListener("click", function(){
+
 	form_3.style.display = "none";
 	form_4.style.display = "block";
-
 	form_4_btns.style.display = "flex";
 	form_3_btns.style.display = "none";
-
 	form_4_progessbar.classList.add("active");
 });
 
 
 form_4_back_btn.addEventListener("click", function(){
+
 	form_3.style.display = "block";
 	form_4.style.display = "none";
-
 	form_4_btns.style.display = "none";
 	form_3_btns.style.display = "flex";
-
 	form_4_progessbar.classList.remove("active");
 });
 
 form_4_next_btn.addEventListener("click", function(){
-	form_4.style.display = "none";
-	form_5.style.display = "block";
-
-	form_5_btns.style.display = "flex";
-	form_4_btns.style.display = "none";
-
-	form_5_progessbar.classList.add("active");
+	var selectsos = document.getElementById('socioAet')
+	if (selectsos.value  == 0){
+		alert ("Complete los campos")
+	}
+	else{
+		form_4.style.display = "none";
+		form_5.style.display = "block";
+		form_5_btns.style.display = "flex";
+		form_4_btns.style.display = "none";
+		form_5_progessbar.classList.add("active");}
 });
 
 form_5_back_btn.addEventListener("click", function(){
+	
 	form_4.style.display = "block";
 	form_5.style.display = "none";
-
 	form_5_btns.style.display = "none";
 	form_4_btns.style.display = "flex";
-
 	form_5_progessbar.classList.remove("active");
 });
 
