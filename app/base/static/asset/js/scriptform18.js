590d741ec03780296a467c8ba556858716ea29c2
kaponero <kaponero@gmail.com>
2023-08-11 16:54:39 -0300

    validación de todos los campos

--- app/base/static/asset/js/scriptform18.js
+++ app/base/static/asset/js/scriptform18.js
@@ -69,17 +69,26 @@ form_2_back_btn.addEventListener("click", function(){
 	form_1_btns.style.display = "flex";
 	form_2_btns.style.display = "none";
 	form_2_progessbar.classList.remove("active");
-	
 });
 
 form_2_next_btn.addEventListener("click", function(){
 	validate()
-	form_2.style.display = "none";
-	form_3.style.display = "block";
-	form_3_btns.style.display = "flex";
-	form_2_btns.style.display = "none";
-	form_3_progessbar.classList.add("active");
-	
+	var inputrutina = document.getElementById('linkRutina').value
+	var inputvideo1 = document.getElementById('linkVideo1').value
+	var inputvideo2 = document.getElementById('linkVideo2').value
+	var inputvideo3 = document.getElementById('linkVideo3').value
+	var inputcover= document.getElementById('linkCover').value
+
+	if (inputrutina.length == 0 || inputvideo1.length  == 0 || inputvideo2.length == 0 || 
+		inputvideo3.length == 0 || inputcover.length == 0 ){
+		alert ("Complete los campos")
+	}
+	else{
+		form_2.style.display = "none";
+		form_3.style.display = "block";
+		form_3_btns.style.display = "flex";
+		form_2_btns.style.display = "none";
+		form_3_progessbar.classList.add("active");}
 });
 
 form_3_back_btn.addEventListener("click", function(){
@@ -93,11 +102,30 @@ form_3_back_btn.addEventListener("click", function(){
 
 form_3_next_btn.addEventListener("click", function(){
 	validate()
-	form_3.style.display = "none";
-	form_4.style.display = "block";
-	form_4_btns.style.display = "flex";
-	form_3_btns.style.display = "none";
-	form_4_progessbar.classList.add("active");
+	var inputproductor = document.getElementById('nombreProductor').value
+	var inputcoproductor = document.getElementById('nombreCoproductor').value
+	var inputautor = document.getElementById('nombreAutor').value
+	var inputeditor = document.getElementById('nombreEditor').value
+	var inputdirector = document.getElementById('nombreDirector').value
+	var inputcamara = document.getElementById('nombreCamara').value
+	var inputsonido = document.getElementById('nombreSonido').value
+	var inputconductor = document.getElementById('nombreConductor').value
+	var inputprotagonista = document.getElementById('nombreProtagonista').value
+	var inputcronista = document.getElementById('nombreCronista').value
+
+	if (inputproductor.length == 0 || inputcoproductor.length  == 0 || inputautor.length == 0 || 
+		inputeditor.length == 0 || inputdirector.length  == 0 || inputcamara.length  == 0 || 
+		inputsonido.length == 0 || inputconductor.length  == 0 || inputprotagonista.length  == 0 || 
+		inputcronista.length == 0){
+		alert ("Complete los campos")
+	}
+	else{
+		form_3.style.display = "none";
+		form_4.style.display = "block";
+		form_4_btns.style.display = "flex";
+		form_3_btns.style.display = "none";
+		form_4_progessbar.classList.add("active");
+	}
 });
 
 
@@ -111,9 +139,21 @@ form_4_back_btn.addEventListener("click", function(){
 });
 
 form_4_next_btn.addEventListener("click", function(){
+	var correo= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
 	validate()
-	var selectsos = document.getElementById('socioAet')
-	if (selectsos.value  == 0){
+	var inputnombrec= document.getElementById('nombreCanal').value
+	var inputdireccionc = document.getElementById('direccionCanal').value
+	var inputlocalidadc = document.getElementById('localidadCanal').value
+	var inputcontactoc = document.getElementById('contactoCanal').value
+	var inputtelefonoc = document.getElementById('telefonoCanal').value
+	var inputemailc = document.getElementById('emailCanal').value
+	var selectsos = document.getElementById('socioAet').value
+	var correovalido = correo.test(inputemailc);
+
+	if (inputnombrec.length == 0 || inputdireccionc.length == 0 || inputlocalidadc.length == 0 ||
+		inputcontactoc .length == 0 || inputtelefonoc.length == 0 || correovalido == false ||
+		selectsos == 0){
 		alert ("Complete los campos")
 	}
 	else{
@@ -134,8 +174,15 @@ form_5_back_btn.addEventListener("click", function(){
 });
 
 btn_done.addEventListener("click", function(){
-	
-	document.getElementById('form1').submit();
+	var inputrazons = document.getElementById('razonSocial').value
+	var inputcuit = document.getElementById('numeroCuit').value
+
+	if (inputrazons.length == 0 || inputcuit.length == 0){
+		alert ("Complete los campos")
+	}
+	else{
+		document.getElementById('form1').submit();
+	}
 })
 
 shadow.addEventListener("click", function(){
