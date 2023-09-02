2148234e57b8d87b249e5dad9c4eece1725053c2
kaponero <kaponero@gmail.com>
2023-09-02 16:46:22 -0300

    reestructuracion formulario

--- app/base/static/asset/js/scriptform.js
+++ app/base/static/asset/js/scriptform.js
@@ -36,31 +36,24 @@ var telefono = /^[0-9]{3}[0-9]{7}|[0-9]{7}$/
 
 
 form_1_next_btn.addEventListener("click", function(){
-	
-	var inputtnom = document.getElementById('nombrePrograma').value
+
 	var selectsos = document.getElementById('socio').value
 	var selectcat = document.getElementById('localidadCategoria').value
 	var selectcat2 = document.getElementById('localidadCategoria2').value
 	var selectgen = document.getElementById('generoPrograma').value
-	var selectviv = document.getElementById('enVivo').value
-	var inputlocal= document.getElementById('localidadEmision').value
-	var inputdate= document.getElementById('input_date').value
-	var inputtime= document.getElementById('input_time').value
-	var inputtarea= document.getElementById('input_tarea').value
+	var inputrazons = document.getElementById('razonSocial').value
+	var inputcuit = document.getElementById('numeroCuit').value
+	var cuilvalido = cuil.test(inputcuit);
 
-	valid_input('nombrePrograma', 'Ingrese el nombe del programa', 'error-nombrePrograma');
 	valid_input('socio', 'Seleccione una opción', 'error-socioAet');
 	valid_input('localidadCategoria', 'Selecione una categoria', 'error-localidadCategoria');
 	valid_input('localidadCategoria2', 'Selecione una categoria', 'error-localidadCategoria2');
 	valid_input('generoPrograma', 'Seleccione un género', 'error-genero');
-	valid_input('enVivo', 'Elija una opción', 'error-vivo');
-	valid_input('localidadEmision', 'Ingrese localidad de emisión', 'error-localidad');
-	valid_input('input_date', 'Ingrese fecha de emisión', 'error-fecha');
-	valid_input('input_time', 'Ingrese duración', 'error-duracion');
-	valid_input('input_tarea', 'Ingrese una breve descripción', 'error-tarea');
+	valid_input('razonSocial', 'ingrese la razon social', 'error-razonSocial');
+	valid_cuil('numeroCuit', 'ingrese el cuit sin espacios', 'error-numeroCuit');
 	
-	if (inputtnom.length == 0 || selectsos == 0 || (selectcat  == 0 &&  selectcat2 == 0) || selectgen == 0 || selectviv == 0 || 
-		inputlocal.length == 0 || inputdate.length == 0 || inputtime.length == 0 || inputtarea.length == 0){
+	if (selectsos == 0 || (selectcat  == 0 &&  selectcat2 == 0) || selectgen == 0 || 
+	inputrazons.length == 0 || cuilvalido == false){
 		alert ("Complete los campos")
 	}
 	else{
@@ -81,21 +74,23 @@ form_2_back_btn.addEventListener("click", function(){
 });
 
 form_2_next_btn.addEventListener("click", function(){
+
+	var inputtnom = document.getElementById('nombrePrograma').value
+	var selectviv = document.getElementById('enVivo').value
+	var inputlocal= document.getElementById('localidadEmision').value
+	var inputdate= document.getElementById('input_date').value
+	var inputtime= document.getElementById('input_time').value
+	var inputtarea= document.getElementById('input_tarea').value
+
+	valid_input('nombrePrograma', 'Ingrese el nombe del programa', 'error-nombrePrograma');
+	valid_input('enVivo', 'Elija una opción', 'error-vivo');
+	valid_input('localidadEmision', 'Ingrese localidad de emisión', 'error-localidad');
+	valid_input('input_date', 'Ingrese fecha de emisión', 'error-fecha');
+	valid_input('input_time', 'Ingrese duración', 'error-duracion');
+	valid_input('input_tarea', 'Ingrese una breve descripción', 'error-tarea');
 	
-	var inputrutina = document.getElementById('linkRutina').value
-	var inputvideo1 = document.getElementById('linkVideo1').value
-	var inputvideo2 = document.getElementById('linkVideo2').value
-	var inputvideo3 = document.getElementById('linkVideo3').value
-	var inputcover= document.getElementById('linkCover').value
-
-	valid_input('linkRutina', 'Ingrese link de la rutina', 'error-rutina');
-	valid_input('linkVideo1', 'Ingrese link del video 1', 'error-video1');
-	valid_input('linkVideo2', 'Ingrese link del video 2', 'error-video2');
-	valid_input('linkVideo3', 'Ingrese link del video 3', 'error-video3');
-	valid_input('linkCover', 'Ingrese link del cover', 'error-cover');
-
-	if (inputrutina.length == 0 || inputvideo1.length  == 0 || inputvideo2.length == 0 || 
-		inputvideo3.length == 0 || inputcover.length == 0 ){
+	if (inputtnom.length == 0  || selectviv == 0 || inputlocal.length == 0 || 
+		inputdate.length == 0 || inputtime.length == 0 || inputtarea.length == 0){
 		alert ("Complete los campos")
 	}
 	else{
@@ -117,32 +112,20 @@ form_3_back_btn.addEventListener("click", function(){
 
 form_3_next_btn.addEventListener("click", function(){
 	
-	var inputproductor = document.getElementById('nombreProductor').value
-	var inputcoproductor = document.getElementById('nombreCoproductor').value
-	var inputautor = document.getElementById('nombreAutor').value
-	var inputeditor = document.getElementById('nombreEditor').value
-	var inputdirector = document.getElementById('nombreDirector').value
-	var inputcamara = document.getElementById('nombreCamara').value
-	var inputsonido = document.getElementById('nombreSonido').value
-	var inputconductor = document.getElementById('nombreConductor').value
-	var inputprotagonista = document.getElementById('nombreProtagonista').value
-	var inputcronista = document.getElementById('nombreCronista').value
+	var inputrutina = document.getElementById('linkRutina').value
+	var inputvideo1 = document.getElementById('linkVideo1').value
+	var inputvideo2 = document.getElementById('linkVideo2').value
+	var inputvideo3 = document.getElementById('linkVideo3').value
+	var inputcover= document.getElementById('linkCover').value
 
-	valid_input('nombreProductor', 'Ingrese el nombre del Productor', 'error-nombreProductor');
-	valid_input('nombreCoproductor', 'Ingrese el nombre del Coproductor', 'error-nombreCoproductor');
-	valid_input('nombreAutor', 'Ingrese el nombre del Autor', 'error-nombreAutor');
-	valid_input('nombreEditor', 'Ingrese el nombre del Editor', 'error-nombreEditor');
-	valid_input('nombreDirector', 'Ingrese el nombre del Director', 'error-nombreDirector');
-	valid_input('nombreCamara', 'Ingrese el nombre del Camarógrafo', 'error-nombreCamara');
-	valid_input('nombreSonido', 'Ingrese datos del Sonidista', 'error-nombreSonido');
-	valid_input('nombreConductor', 'Ingrese el nombre del Conductor/Presentador/Locutor', 'error-nombreConductor');
-	valid_input('nombreProtagonista', 'Ingrese el nombre del Protagonista', 'error-nombreProtagonista');
-	valid_input('nombreCronista', 'Ingrese el nombre del Cronista', 'error-nombreCronista');
+	valid_input('linkRutina', 'Ingrese link de la rutina', 'error-rutina');
+	valid_input('linkVideo1', 'Ingrese link del video 1', 'error-video1');
+	valid_input('linkVideo2', 'Ingrese link del video 2', 'error-video2');
+	valid_input('linkVideo3', 'Ingrese link del video 3', 'error-video3');
+	valid_input('linkCover', 'Ingrese link del cover', 'error-cover');
 	
-	if (inputproductor.length == 0 || inputcoproductor.length  == 0 || inputautor.length == 0 || 
-		inputeditor.length == 0 || inputdirector.length  == 0 || inputcamara.length  == 0 || 
-		inputsonido.length == 0 || inputconductor.length  == 0 || inputprotagonista.length  == 0 || 
-		inputcronista.length == 0){
+	if (inputrutina.length == 0 || inputvideo1.length  == 0 || inputvideo2.length == 0 || 
+		inputvideo3.length == 0 || inputcover.length == 0 ){
 		alert ("Complete los campos")
 	}
 	else{
@@ -165,25 +148,32 @@ form_4_back_btn.addEventListener("click", function(){
 
 form_4_next_btn.addEventListener("click", function(){
 	
-	var inputnombrec= document.getElementById('nombreCanal').value
-	var inputdireccionc = document.getElementById('direccionCanal').value
-	var inputlocalidadc = document.getElementById('localidadCanal').value
-	var inputcontactoc = document.getElementById('contactoCanal').value
-	var inputtelefonoc = document.getElementById('telefonoCanal').value
-	var inputemailc = document.getElementById('emailCanal').value
-	var correovalido = correo.test(inputemailc);
-	var telvalido = telefono.test(inputtelefonoc);
+	var inputproductor = document.getElementById('nombreProductor').value
+	var inputcoproductor = document.getElementById('nombreCoproductor').value
+	var inputautor = document.getElementById('nombreAutor').value
+	var inputeditor = document.getElementById('nombreEditor').value
+	var inputdirector = document.getElementById('nombreDirector').value
+	var inputcamara = document.getElementById('nombreCamara').value
+	var inputsonido = document.getElementById('nombreSonido').value
+	var inputconductor = document.getElementById('nombreConductor').value
+	var inputprotagonista = document.getElementById('nombreProtagonista').value
+	var inputcronista = document.getElementById('nombreCronista').value
 
-	valid_input('nombreCanal', 'Ingrese el nombre del Canal', 'error-nombreCanal');
-	valid_input('direccionCanal', 'Ingrese el domicilio del Canal', 'error-direccionCanal');
-	valid_input('localidadCanal', 'Ingrese la localidad del Canal', 'error-localidadCanal');
-	valid_input('contactoCanal', 'Ingrese un medio de contacto', 'error-contactoCanal');
-	valid_tel('telefonoCanal', 'Ingrese el teléfono del Canal', 'error-telefonoCanal');
-	valid_correo('emailCanal', 'Ingrese un correo valido', 'error-emailCanal');
-	
+	valid_input('nombreProductor', 'Ingrese el nombre del Productor', 'error-nombreProductor');
+	valid_input('nombreCoproductor', 'Ingrese el nombre del Coproductor', 'error-nombreCoproductor');
+	valid_input('nombreAutor', 'Ingrese el nombre del Autor', 'error-nombreAutor');
+	valid_input('nombreEditor', 'Ingrese el nombre del Editor', 'error-nombreEditor');
+	valid_input('nombreDirector', 'Ingrese el nombre del Director', 'error-nombreDirector');
+	valid_input('nombreCamara', 'Ingrese el nombre del Camarógrafo', 'error-nombreCamara');
+	valid_input('nombreSonido', 'Ingrese datos del Sonidista', 'error-nombreSonido');
+	valid_input('nombreConductor', 'Ingrese el nombre del Conductor/Presentador/Locutor', 'error-nombreConductor');
+	valid_input('nombreProtagonista', 'Ingrese el nombre del Protagonista', 'error-nombreProtagonista');
+	valid_input('nombreCronista', 'Ingrese el nombre del Cronista', 'error-nombreCronista');
 
-	if (inputnombrec.length == 0 || inputdireccionc.length == 0 || inputlocalidadc.length == 0 ||
-		inputcontactoc .length == 0 || telvalido == false  || correovalido == false){
+	if (inputproductor.length == 0 || inputcoproductor.length  == 0 || inputautor.length == 0 || 
+		inputeditor.length == 0 || inputdirector.length  == 0 || inputcamara.length  == 0 || 
+		inputsonido.length == 0 || inputconductor.length  == 0 || inputprotagonista.length  == 0 || 
+		inputcronista.length == 0){
 		alert ("Complete los campos")
 	}
 	else{
@@ -205,17 +195,26 @@ form_5_back_btn.addEventListener("click", function(){
 
 btn_done.addEventListener("click", function(){
 
-	var inputrazons = document.getElementById('razonSocial').value
-	var inputcuit = document.getElementById('numeroCuit').value
+	var inputnombrec= document.getElementById('nombreCanal').value
+	var inputdireccionc = document.getElementById('direccionCanal').value
+	var inputlocalidadc = document.getElementById('localidadCanal').value
+	var inputcontactoc = document.getElementById('contactoCanal').value
+	var inputtelefonoc = document.getElementById('telefonoCanal').value
+	var inputemailc = document.getElementById('emailCanal').value
+	var correovalido = correo.test(inputemailc);
+	var telvalido = telefono.test(inputtelefonoc);
 	var inputcheck = document.getElementById('condiciones').checked
 
-	var cuilvalido = cuil.test(inputcuit);
-
-	valid_input('razonSocial', 'ingrese la razon social', 'error-razonSocial');
-	valid_cuil('numeroCuit', 'ingrese el cuit sin espacios', 'error-numeroCuit');
+	valid_input('nombreCanal', 'Ingrese el nombre del Canal', 'error-nombreCanal');
+	valid_input('direccionCanal', 'Ingrese el domicilio del Canal', 'error-direccionCanal');
+	valid_input('localidadCanal', 'Ingrese la localidad del Canal', 'error-localidadCanal');
+	valid_input('contactoCanal', 'Ingrese un medio de contacto', 'error-contactoCanal');
+	valid_tel('telefonoCanal', 'Ingrese el teléfono del Canal', 'error-telefonoCanal');
+	valid_correo('emailCanal', 'Ingrese un correo valido', 'error-emailCanal');
 	valid_condiciones('condiciones', 'Debe aceptar los terminos y condiciones', 'error-condiciones');
-
-	if (inputrazons.length == 0 || cuilvalido == false || inputcheck==false){
+	
+	if (inputnombrec.length == 0 || inputdireccionc.length == 0 || inputlocalidadc.length == 0 ||
+		inputcontactoc .length == 0 || telvalido == false  || correovalido == false || inputcheck==false){
 		alert ("Complete los campos")
 	}
 	else{
@@ -295,17 +294,20 @@ function valid_condiciones(identificador,mensaje, diverror){
 function ver_categoria(){
 	var inputtnom = document.getElementById("socio");
 	var es_socio = document.getElementById("es_socio");
+	var es_socio2 = document.getElementById("es_socio2");
 	var no_es_socio = document.getElementById("no_es_socio");
 	var selectcat = document.getElementById('localidadCategoria')
 	var selectcat2 = document.getElementById('localidadCategoria2')
 
 	if (inputtnom.options[inputtnom.selectedIndex].value == "si"){
 		es_socio.style.display = 'block';
+		es_socio2.style.display = 'block';
 		no_es_socio.style.display = 'none';
 		selectcat2.selectedIndex = "";
 	}
 	else{
 		es_socio.style.display = 'none';
+		es_socio2.style.display = 'none';
 		no_es_socio.style.display = 'block';
 		selectcat.selectedIndex = "";
 	}
